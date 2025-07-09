0f0ab826d8e099b501955d1883624d8ae2bbc705
imranrasulzade <imranrasulzade5@gmail.com>
2025-07-09 20:07:06 +0400

    first commit

---
+++ src/main/java/com/example/springbootcicddemo/SpringBootCiCdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootcicddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCiCdDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCiCdDemoApplication.class, args);
+    }
+
+}
