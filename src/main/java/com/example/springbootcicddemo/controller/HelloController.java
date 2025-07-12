d6029dbb581027f45260b0b864f5d384ff220c0f
imranrasulzade <imranrasulzade5@gmail.com>
2025-07-12 10:14:18 +0400

    some add

--- src/main/java/com/example/springbootcicddemo/controller/HelloController.java
+++ src/main/java/com/example/springbootcicddemo/controller/HelloController.java
@@ -13,6 +13,7 @@ public class HelloController {
     @GetMapping
     public String sayHello() {
         System.out.println("Hello World");
+        System.out.println("Some");
         return "Hello World!";
     }
 
